feba04fba1337155cbfd5bc8c4124365bfc11040
Andrii Telishko <telishkoav@gmail.com>
2021-06-01 00:19:38 +0300

    done task 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Profile from "./components/Profile/Profile"
 import user from "./json/user.json"
+import Statistics from "./components/Statistics/Statistics"
+import statistic from "./json/statistical-data.json"
 
 function App() {
   return (
+    <>
     <Profile
       name={user.name}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
-  stats={user.stats}/>
+      stats={user.stats} />
+      <Statistics
+        title={'Upload stats'}
+        stats = {statistic}
+      />
+      </>
   );
 }
 
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,1 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Statistics.module.css"
+
+
+const Statistics = ({title, stats}) => (
+    <section className={styles.statistic_section}>
+        <h2>Statistics</h2>
+        {title && (<h3 className={styles.section_title}>{title}</h3>)}
+        <ul className={styles.statistic_list}>
+            {stats.map(({ id, label, percentage }) => (
+                <li key={id} className={styles.list_item}
+                    style={{ backgroundColor: "#" + Math.floor(Math.random() * 16777215).toString(16) }}>
+                    <span className={styles.item_label}>{label}</span>
+                    <span className={styles.item_quantity}>{percentage}%</span>
+                </li>
+            ))}
+        </ul>
+    </section>
+)
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.array.isRequired
+}
+
+export default Statistics
